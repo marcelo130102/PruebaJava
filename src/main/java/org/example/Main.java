64913405665a3ac786bbbb33c3b7bf5c32f34931
Marcelo Surco <marcelo.surco@utec.edu.pe>
2022-05-21 18:01:23 -0500

    Quitando code smells

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logger logger = Logger.getLogger(Main.class.getName());
+
+        // log messages using log(Level level, String msg)
+        logger.log(Level.INFO, "This is message 1");
+        logger.log(Level.WARNING, "This is message 2");
+
     }
 }
